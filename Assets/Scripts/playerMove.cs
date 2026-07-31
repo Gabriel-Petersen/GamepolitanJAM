8f203a000f7b60e7feceed74026a9b04d9038fb7
cezimbra <rafael.rodrigues@aln.senaicimatec.edu.br>
2026-07-31 19:31:41 -0300

    cena ceziCena, inicio do script de movimentacao

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class playerMove : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-}
+        // 1. Digital Keys (true only on the frame it is pressed)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        // 3. Virtual Axes (Returns a smoothed float value between -1.0 and 1.0)
+        // Uses presets configured in Edit > Project Settings > Input Manager
+        float horizontal = Input.GetAxis("Horizontal"); // Smooth filtering (WASD/Left Stick)
+        float vertical = Input.GetAxis("Vertical"); 
+        //float vertical = Input.GetAxisRaw("Vertical");    // Snappy, unfiltered (-1, 0, or 1)
+
+    void Jump()
+        {
+
+        }
+
+    void Walk()
+        {
+
+        }
