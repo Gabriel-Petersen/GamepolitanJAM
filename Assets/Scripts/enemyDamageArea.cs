a69fccf8905b40d5ad68ff5c0ba5f4eba8d4143a
cezimbra <rafael.rodrigues@aln.senaicimatec.edu.br>
2026-08-01 15:17:23 -0300

    inicio do script de ataque do inimigo

--- Assets/Scripts/enemyDamageArea.cs
+++ Assets/Scripts/enemyDamageArea.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class enemyDamageArea : MonoBehaviour
 {
+
+   
+    public float cooldownTime = 1f;
+    private float lastUsedTime = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lastUsedTime = -cooldownTime;
     }
 
     // Update is called once per frame
@@ -14,13 +18,18 @@ public class enemyDamageArea : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider collision)
+    void OnTriggerStay(Collider collision)
     {
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
-        HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
-        if (healthSystem != null && healthSystem.isPlayer)
+        if(lastUsedTime + cooldownTime < Time.time)
         {
-            healthSystem.ChangeHp(-1);
+            Debug.Log("Collision detected with: " + collision.gameObject.name);
+            HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
+            if (healthSystem != null && healthSystem.isPlayer)
+            {
+                healthSystem.ChangeHp(-1);
+            }
+            lastUsedTime = Time.time;
         }
+        
     }
 }
