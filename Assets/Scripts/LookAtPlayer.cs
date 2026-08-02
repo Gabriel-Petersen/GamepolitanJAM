4613130bd1788002113d6dcfab7a7feb2b21f796
cezimbra <rafael.rodrigues@aln.senaicimatec.edu.br>
2026-08-02 01:33:43 -0300

    arvores olham para o player e vidro transparente

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Camera pc;
+    Vector3 direction;
     void Start()
     {
-        
+         pc = FindFirstObjectByType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = pc.gameObject.transform.position - transform.position;
+
+        // Only look at the player on the Y axis (horizontal), keep X axis fixed
+        direction.y = 0;
+        transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(transform.eulerAngles.x, 0, 0);
     }
 }
